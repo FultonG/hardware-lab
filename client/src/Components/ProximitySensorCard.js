e480bf00661031c155927890637af0f354aa6634
Fulton Garcia <fultongarcia@gmail.com>
2020-10-11 04:48:30 -0400

    turning off and on monitoring

--- client/src/Components/ProximitySensorCard.js
+++ client/src/Components/ProximitySensorCard.js
@@ -12,9 +12,16 @@ const Code = styled.code`
 
 const ProximitySensorCard = ({ initalSensor }) => {
   const [sensor, setSensor] = useState(initalSensor)
+  const [monitoring, setMonitoring] = useState(true);
+  const [intervalState, setIntervalState] = useState(null);
   useEffect(() => {
-    setInterval(fetchSensorData, 100);
-  }, []);
+    if(monitoring){
+      setIntervalState(setInterval(fetchSensorData, 100));
+    } else {
+      clearInterval(intervalState);
+      setIntervalState(null);
+    }
+  }, [monitoring]);
 
   const fetchSensorData = async () => {
     try {
@@ -23,11 +30,10 @@ const ProximitySensorCard = ({ initalSensor }) => {
     } catch(e){
       console.log(e.message);
     }
-    
-    
   }
+
   return (
-    <Card padding="20px" width="30%" height="70%" margin="10px 10px 0px 0px">
+    <Card padding="20px" width="30%" height="100%" margin="10px 10px 0px 0px" onClick={() => setMonitoring(prev => !prev)} hover cursor="pointer">
       <Container justify="space-around" align="unset" direction="column" >
         <Text>Name: <Code>{sensor.name}</Code></Text>
         <Text>Status: <Code>{sensor.status}</Code></Text>
@@ -38,6 +44,9 @@ const ProximitySensorCard = ({ initalSensor }) => {
             <Text>Distance: <Code>{sensor.centimeters}cm/{sensor.inches}in</Code></Text>
           </>
         )}
+        <Text>Monitoring: {!monitoring ? 
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/92/Location_dot_red.svg" style={{width: "15px", height: '15px'}}/> : 
+        <img src="https://www.clker.com/cliparts/c/7/U/N/A/T/green-dot-small.svg.med.png" style={{width: "15px", height: '15px'}}/> }</Text>
       </Container>
     </Card>
   )
